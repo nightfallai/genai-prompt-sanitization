def336b696f3fd91408c305e674757d21424f0b7
Ben Lambert <ben.lambert@nightfall.ai>
2024-07-08 17:47:46 -0400

    reformat with black

--- claude-example.py
+++ claude-example.py
@@ -2,8 +2,14 @@ import os
 
 from anthropic import Anthropic
 from dotenv import load_dotenv
-from nightfall import (Confidence, DetectionRule, Detector, MaskConfig,
-                       Nightfall, RedactionConfig)
+from nightfall import (
+    Confidence,
+    DetectionRule,
+    Detector,
+    MaskConfig,
+    Nightfall,
+    RedactionConfig,
+)
 
 # Load environment variables
 load_dotenv()
@@ -11,7 +17,7 @@ load_dotenv()
 # Initialize clients
 try:
     # By default Nightfall will read the NIGHTFALL_API_KEY environment variable
-    nightfall = Nightfall()  
+    nightfall = Nightfall()
 
     # Initialize the Anthropic client with your API key
     client = Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
@@ -27,27 +33,31 @@ payload = [user_input]
 print("\nHere's the user's question before sanitization:\n", user_input)
 
 # Define an inline detection rule that looks for Likely Credit Card Numbers and redacts them
-detection_rule = [DetectionRule(
-    [Detector(
-        min_confidence=Confidence.VERY_LIKELY,
-        nightfall_detector="CREDIT_CARD_NUMBER",
-        display_name="Credit Card Number",
-        redaction_config=RedactionConfig(
-            remove_finding=False,
-            mask_config=MaskConfig(
-                masking_char="X",
-                num_chars_to_leave_unmasked=4,
-                mask_right_to_left=True,
-                chars_to_ignore=["-"])
-        )
-    )]
-)]
+detection_rule = [
+    DetectionRule(
+        [
+            Detector(
+                min_confidence=Confidence.VERY_LIKELY,
+                nightfall_detector="CREDIT_CARD_NUMBER",
+                display_name="Credit Card Number",
+                redaction_config=RedactionConfig(
+                    remove_finding=False,
+                    mask_config=MaskConfig(
+                        masking_char="X",
+                        num_chars_to_leave_unmasked=4,
+                        mask_right_to_left=True,
+                        chars_to_ignore=["-"],
+                    ),
+                ),
+            )
+        ]
+    )
+]
 
 try:
     # Send the message to Nightfall to scan it for sensitive data
     findings, redacted_payload = nightfall.scan_text(
-        payload,
-        detection_rules=detection_rule
+        payload, detection_rules=detection_rule
     )
 
     # If the message has sensitive data, use the redacted version, otherwise use the original message
@@ -56,7 +66,11 @@ try:
     print("\nHere's the user's question after sanitization:\n", user_input_sanitized)
 
     # Define your prompt, ensuring it starts with "\n\nHuman:" and ending with "\n\nAssistant:"
-    prompt = "\nYou are a level 1 support bot. Your role is to assist users with common issues and provide helpful information. \n\nHuman: " + user_input_sanitized + "\n\nAssistant:"
+    prompt = (
+        "\nYou are a level 1 support bot. Your role is to assist users with common issues and provide helpful information. \n\nHuman: "
+        + user_input_sanitized
+        + "\n\nAssistant:"
+    )
 
     # Send prompt to Anthropic model for AI-generated response
     response = client.completions.create(
@@ -64,10 +78,13 @@ try:
         prompt=prompt,
         max_tokens_to_sample=1024,
         temperature=0.7,
-        top_p=1.0
+        top_p=1.0,
     )
 
-    print("\nHere's a generated response you can send the customer:\n", response.completion)
+    print(
+        "\nHere's a generated response you can send the customer:\n",
+        response.completion,
+    )
 
 except Exception as e:
     print(f"An error occurred: {e}")
--- langchain-example.py
+++ langchain-example.py
@@ -8,8 +8,14 @@ from langchain.schema.language_model import BaseLanguageModel
 from langchain.schema.prompt_template import BasePromptTemplate
 from langchain.schema.runnable import RunnablePassthrough, RunnableSequence
 from langchain_anthropic import ChatAnthropic
-from nightfall import (Confidence, DetectionRule, Detector, MaskConfig,
-                       Nightfall, RedactionConfig)
+from nightfall import (
+    Confidence,
+    DetectionRule,
+    Detector,
+    MaskConfig,
+    Nightfall,
+    RedactionConfig,
+)
 from pydantic import Field
 
 # Load environment variables
@@ -19,21 +25,27 @@ load_dotenv()
 nightfall = Nightfall()
 
 # Define Nightfall detection rule
-detection_rule = [DetectionRule(
-    [Detector(
-        min_confidence=Confidence.VERY_LIKELY,
-        nightfall_detector="CREDIT_CARD_NUMBER",
-        display_name="Credit Card Number",
-        redaction_config=RedactionConfig(
-            remove_finding=False,
-            mask_config=MaskConfig(
-                masking_char="X",
-                num_chars_to_leave_unmasked=4,
-                mask_right_to_left=True,
-                chars_to_ignore=["-"])
-        )
-    )]
-)]
+detection_rule = [
+    DetectionRule(
+        [
+            Detector(
+                min_confidence=Confidence.VERY_LIKELY,
+                nightfall_detector="CREDIT_CARD_NUMBER",
+                display_name="Credit Card Number",
+                redaction_config=RedactionConfig(
+                    remove_finding=False,
+                    mask_config=MaskConfig(
+                        masking_char="X",
+                        num_chars_to_leave_unmasked=4,
+                        mask_right_to_left=True,
+                        chars_to_ignore=["-"],
+                    ),
+                ),
+            )
+        ]
+    )
+]
+
 
 class NightfallSanitizationChain(Chain):
     input_key: str = "input"
@@ -52,8 +64,7 @@ class NightfallSanitizationChain(Chain):
         payload = [text]
         try:
             findings, redacted_payload = nightfall.scan_text(
-                payload,
-                detection_rules=detection_rule
+                payload, detection_rules=detection_rule
             )
             sanitized_text = redacted_payload[0] if redacted_payload[0] else text
             print(f"\nsanitized input:\n {sanitized_text}")
@@ -61,6 +72,7 @@ class NightfallSanitizationChain(Chain):
             print(f"Error in sanitizing input: {e}")
             sanitized_text = text
         return {self.output_key: sanitized_text}
+
 
 # Initialize the Anthropic LLM
 llm = ChatAnthropic(model="claude-2.1")
@@ -74,15 +86,17 @@ sanitization_chain = NightfallSanitizationChain()
 
 # Create the full chain using RunnableSequence
 full_chain = (
-    RunnablePassthrough() |
-    sanitization_chain |
-    (lambda x: {"customer_input": x["sanitized_input"]}) |
-    prompt |
-    llm
+    RunnablePassthrough()
+    | sanitization_chain
+    | (lambda x: {"customer_input": x["sanitized_input"]})
+    | prompt
+    | llm
 )
 
 # Use the combined chain
-customer_input = "My credit card number is 4916-6734-7572-5015, and the card is getting declined."
+customer_input = (
+    "My credit card number is 4916-6734-7572-5015, and the card is getting declined."
+)
 print(f"\ncustomer input:\n {customer_input}")
 try:
     response = full_chain.invoke({"input": customer_input})
