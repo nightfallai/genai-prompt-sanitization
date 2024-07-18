6e5e7712b7e0cddff02fca8e94cf664ad3dbf4d5
Colin Shanahan <colin@nightfall.ai>
2024-07-18 14:59:37 -0700

    print spacing

--- examples/openai_example.py
+++ examples/openai_example.py
@@ -9,15 +9,11 @@ from nightfall import (
 )
 from openai import OpenAI
 
-# Load environment variables
+# Load environment variables and initialize clients
 # To run this script you must have a NIGHTFALL_API_KEY and OPENAI_API_KEY
 load_dotenv()
-
-# By default Nightfall will read the NIGHTFALL_API_KEY environment variable
-nf_client = Nightfall()
-
-# By default OpenAI will read the OPENAI_API_KEY environment variable
-oai_client = OpenAI()
+nightfall_client = Nightfall()
+openai_client = OpenAI()
 
 # The prompt you intend to send to OpenAI
 user_input = """The customer said: 'My credit card number is 
@@ -25,7 +21,7 @@ user_input = """The customer said: 'My credit card number is
 How should I respond to the customer?"""
 payload = [user_input]
 
-print(f"\nHere's the user's question before sanitization:\n{user_input}")
+print(f"\nHere's the user's question before sanitization:\n {user_input}")
 
 # Define an inline Nightfall detection rule that looks for 
 # Credit Card Numbers and redacts them with a string of "X"s
@@ -40,7 +36,7 @@ detection_rule = [
                     remove_finding=False,
                     mask_config=MaskConfig(
                         masking_char="X",
-                        num_chars_to_leave_unmasked=4,
+                        num_chars_to_leave_unmasked=1,
                         mask_right_to_left=True,
                         chars_to_ignore=["-"],
                     ),
@@ -53,28 +49,25 @@ detection_rule = [
 # Send the message to Nightfall to scan it for sensitive data.
 # Nightfall returns the sensitive findings and a copy of your input payload with
 # sensitive data redacted.
-findings, redacted_payload = nf_client.scan_text(
+findings, redacted_payload = nightfall_client.scan_text(
     payload, detection_rules=detection_rule
 )
 
 # If the message has sensitive data, use the redacted version, otherwise use the
 # original message.
-if redacted_payload[0]:
-    user_input_sanitized = redacted_payload[0]
-else:
-    user_input_sanitized = payload[0]
+user_input_sanitized = redacted_payload[0] if redacted_payload[0] else payload[0]
 
-print(f"\nHere's the user's question after sanitization:\n{user_input_sanitized}")
+print(f"\nHere's the user's question after sanitization:\n {user_input_sanitized}")
 
 # Send the sanitized prompt to OpenAI model for AI-generated response.
-completion = oai_client.chat.completions.create(
+completion = openai_client.chat.completions.create(
     model="gpt-4",
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "system", "content": "You are a helpful customer service assistant."},
         {"role": "user", "content": user_input_sanitized},
     ],
     max_tokens=1024,
 )
 
 generated_response = completion.choices[0].message.content
-print(f"\nHere's a generated response you can send the customer:\n{generated_response}")
+print(f"\nHere's a generated response you can send the customer:\n {generated_response}")
